45ead59512ad2c53bc49e6e5b521695a5bc0d887
alireza <ansari09165630639@gmail.com>
2023-03-20 13:53:20 +0330

    last commit

--- ML.py
+++ ML.py
@@ -13,6 +13,7 @@ y=[]#output
 for i in range(0,len(result)):
     x.append(result[i][1:])
     y.append(result[i][0])
-clf=tree.DecisionTreeClassifier()
-clf=clf.fit(x,y)
+print(x[0])
+#clf=tree.DecisionTreeClassifier()
+#clf=clf.fit(x,y)
 print("ok")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+mydb=mysql.connector.connect(host='127.0.0.1',user='root',password='8569')
+    mycursor=mydb.cursor()
